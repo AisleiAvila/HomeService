4457e82b5fd1401572d13c9381324d9a3866cc89
Aislei Ávila de Medeiros <aislei@outlook.com.br>
2025-11-08 14:57:23 +0000

    Ajuste para utilizar o Capacitor

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.nathan.app',
+  appName: 'nathan-home-service-pro',
+  webDir: 'dist'
+};
+
+export default config;
