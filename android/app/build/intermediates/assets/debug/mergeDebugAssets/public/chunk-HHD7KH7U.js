9fddacc25e97baedb42bdf11fe9a68a7c8110521
Aislei Ávila de Medeiros <aislei@outlook.com.br>
2025-11-10 00:08:49 +0000

    Ajuste para device mobile

---
+++ android/app/build/intermediates/assets/debug/mergeDebugAssets/public/chunk-HHD7KH7U.js
@@ -0,0 +1,1 @@
+import{a}from"./chunk-ELF4O7UH.js";import"./chunk-UUTQQLSI.js";export{a as StatusService};
---
+++ ios/App/App/public/chunk-BJFKT52B.js
@@ -0,0 +1,1 @@
+import{a,b,c,d,e}from"./chunk-JENGY3ED.js";import"./chunk-IMPBB4AK.js";export{c as Headers,d as Request,e as Response,b as default,a as fetch};
