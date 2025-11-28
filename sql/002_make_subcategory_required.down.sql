4333166bcf3f20048039c9f8d586243f4dd8160e
Aislei Medeiros <aislei.medeiros@celfocus.com>
2025-11-28 18:41:54 +0000

    Exclusão de arquivos SQL e MD

--- sql/002_make_subcategory_required.down.sql
+++
@@ -1,7 +0,0 @@
--- Removemos a restrição NOT NULL
-ALTER TABLE service_requests
-ALTER COLUMN subcategory_id DROP NOT NULL;
-
--- Removemos a chave estrangeira se existir
-ALTER TABLE service_requests
-DROP CONSTRAINT IF EXISTS fk_service_requests_subcategory;
--- sql/add_receive_sms_notifications_to_users.sql
+++
@@ -1,2 +0,0 @@
--- Migration: Adiciona campo para preferência de comunicados por SMS
-ALTER TABLE users ADD COLUMN receive_sms_notifications BOOLEAN DEFAULT FALSE;
