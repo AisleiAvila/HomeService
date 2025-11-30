c5f07ebfef004e3f0aea620f90e3b9b3589692bc
Aislei Medeiros <aislei.medeiros@celfocus.com>
2025-11-30 22:32:56 +0000

    Ajustes nas variáveis de ambiente

--- src/environments/environment prod.ts
+++ src/environments/environment prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  supabaseAnonKey: "SUA_CHAVE",
+  supabaseAnonKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVxcnZlbmxrcXVoZWFqdXZlZ2d2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTcwNzg4NDgsImV4cCI6MjA3MjY1NDg0OH0.ZdgBkvjC5irHh7E9fagqX_Pu797anPfE8jO91iNDRIc",
   supabaseUrl: "https://uqrvenlkquheajuveggv.supabase.co",
   apiUrl: "https://your-backend-url.com",
   supabaseRestUrl: "https://uqrvenlkquheajuveggv.supabase.co/rest/v1",
