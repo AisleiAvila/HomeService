6348d4476622ea2e73192ee791a0cf66afeecbc3
Aislei Medeiros <aislei.medeiros@celfocus.com>
2025-11-30 23:21:41 +0000

    Ajuste acesso Vercel

--- src/environments/environment prod.ts
+++ src/environments/environment prod.ts
@@ -1,12 +1,11 @@
 export const environment = {
-  production: false,
-  supabaseAnonKey:
-    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVxcnZlbmxrcXVoZWFqdXZlZ2d2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTcwNzg4NDgsImV4cCI6MjA3MjY1NDg0OH0.ZdgBkvjC5irHh7E9fagqX_Pu797anPfE8jO91iNDRIc",
-  supabaseUrl: "https://" + "uqrvenlkquheajuveggv.supabase.co",
-  apiUrl: "https://your-backend-url.com", // Altere para o endpoint real do backend/Supabase Function
+  production: true,
+  supabaseAnonKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVxcnZlbmxrcXVoZWFqdXZlZ2d2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTcwNzg4NDgsImV4cCI6MjA3MjY1NDg0OH0.ZdgBkvjC5irHh7E9fagqX_Pu797anPfE8jO91iNDRIc",
+  supabaseUrl: "https://uqrvenlkquheajuveggv.supabase.co",
+  apiUrl: "https://your-backend-url.com",
   supabaseRestUrl: "https://uqrvenlkquheajuveggv.supabase.co/rest/v1",
-  loginApiUrl: "http://localhost:4002/api/login",
-  confirmEmailApiUrl: "http://localhost:4001/api/confirm-email",
+  loginApiUrl: "https://home-service-nu.vercel.app/api/login",
+  confirmEmailApiUrl: "https://home-service-nu.vercel.app/api/confirm-email",
 };
 
 // Adiciona exportação de tipo para compatibilidade Angular
