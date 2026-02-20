60850e2ea13c8d1e8c276c0c898b797695e07438
Aislei Medeiros <aislei.medeiros+celfocus@celfocus.com>
2026-02-20 19:48:16 +0000

    Utilização do Brevo no envio de e-mails

--- src/version.ts
+++ src/version.ts
@@ -1,3 +1,3 @@
 // Arquivo gerado automaticamente a partir do package.json
 // Não edite manualmente. Use `npm version patch|minor|major`.
-export const APP_VERSION = "1.0.3";
+export const APP_VERSION = "1.0.4";
